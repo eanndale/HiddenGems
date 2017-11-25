96bbde415a481e8e0df36845a5c5e2f654e700b6
Jason Han <hanjason96@gmail.com>
2017-11-25 17:21:33 -0500

    nothing has changed i just need to pull

--- HiddenGems/app.py
+++ HiddenGems/app.py
@@ -2,6 +2,7 @@ from chalice import Chalice
 import pymysql
 import googlemaps
 import json
+import math
 
 app = Chalice(app_name='HiddenGems')
 app.debug = True
@@ -14,10 +15,13 @@ def index():
 
 @app.route('/route', methods=['POST'])
 def route():
-    rds_host = "hiddengemsdb.cp1ydngf7sx0.us-east-1.rds.amazonaws.com"
-    name = "hiddengemsdb"
-    password = "Stargazing1"
-    db_name = "hiddengems"
+
+
+    rds_host = 'hiddengemsdb.cp1ydngf7sx0.us-east-1.rds.amazonaws.com'
+    name = 'HiddenGems'
+    password = 'Stargazing1'
+    db_name = 'hiddengemsdb'
+
 
     # this was causing problems, will fix soon.
     # conn = pymysql.connect(rds_host, user=name, passwd=password, db=db_name, connect_timeout=5)
@@ -25,22 +29,28 @@ def route():
     gmaps = googlemaps.Client(key='AIzaSyDTo1GrHUKKmtBiVw4xBQxD1Uv24R1ypvY')
 
     request = app.current_request
-    points = request.json_body
+    input = request.json_body
 
     # start_address = points["start_address"]
     # end_address = points["end_address"]
 
-    start_lat = float(points["start_lat"])
-    start_long = float(points["start_long"])
-    end_lat = float(points["end_lat"])
-    end_long = float(points["end_long"])
 
-    # note this actually works
-    # directions = gmaps.directions(origin=[start_lat, start_long], destination=[end_lat, end_long], mode="driving")
-    # return directions
+    phone_id = input["phone_id"] # maybe typecast to int
+    start_lat = float(input["start_lat"])
+    start_long = float(input["start_long"])
+    start_date = input["start_date"]
+    end_lat = float(input["end_lat"])
+    end_long = float(input["end_long"])
+    end_date = input["end_date"]
+
+    budget = input["budget"] # maybe typecast to float
+    radius = input["radius"] # maybe typecast to int
+    preferences = input["preferences"]
+
 
     # Really bad implementation of midpoints (not accounting for optimal routing)
     # ------------------------------------------------------------------------------
+
     directions = gmaps.directions(origin=[start_lat, start_long], destination=[end_lat, end_long], mode="driving")
     direct = json.dumps(directions)
     directer = json.loads(direct[1:-1])
@@ -49,24 +59,25 @@ def route():
     miles = float(meters) / 1000.0 * conv_fac
     stops = int(miles / 150) - 1
 
+
     '''
     R = 6373.0 # radius of Earth in km
 
-    lat1 = radians(start_lat)
+    lat1 = math.radians(start_lat)
     
-    long1 = radians(start_long)
-    lat2 = radians(end_lat)
-    long2 = radians(end_long)
+    long1 = math.radians(start_long)
+    lat2 = math.radians(end_lat)
+    long2 = math.radians(end_long)
 
     dlong = long2 - long1
     dlat = lat2 - lat1
 
-    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlong / 2)**2
-    c = 2 * arctan2(sqrt(a), sqrt(1 - a))
+    a =  math.sin(dlat / 2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlong / 2)**2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
 
     distance = R * c
 
-    stops = distance / 150 - 1
+    stops = int(distance / 150) - 1
     '''
 
     results = {
@@ -76,7 +87,7 @@ def route():
     if (stops < 1):
         mid_lat = (start_lat + end_lat) / 2.0
         mid_long = (start_long + end_long) / 2.0
-        nearby = gmaps.places_nearby(keyword = 'attraction', location = [mid_lat, mid_long], radius = 50000)
+        nearby = gmaps.places_nearby(keyword = 'attraction', location = [mid_lat, mid_long], radius = 30000)
         nearest = nearby['results'][0]
         data = {'place_id': nearest['place_id'],
                 'name': nearest['name'],
@@ -92,12 +103,12 @@ def route():
     else:
         mid_lat = start_lat
         mid_long = start_long
-        inc_lat = abs(end_lat - start_lat) / float(stops + 1)
-        inc_long = abs(end_long - start_long) / float(stops + 1)
+        inc_lat = (end_lat - start_lat) / float(stops + 1)
+        inc_long = (end_long - start_long) / float(stops + 1)
         for i in range(0, stops):
             mid_lat += inc_lat
             mid_long += inc_long
-            nearby = gmaps.places_nearby(keyword = 'attraction', location = [mid_lat, mid_long], radius = 50000)
+            nearby = gmaps.places_nearby(keyword = 'attraction', location = [mid_lat, mid_long], radius = 30000)
             if (len(nearby['results']) > 0):
                 nearest = nearby['results'][0]
                 data = {'place_id': nearest['place_id'],
@@ -152,21 +163,24 @@ def update():
 
     request = app.current_request
     place = request.json_body
-    data = {}
 
     nearby = gmaps.places_nearby(keyword = 'attraction', location = [place['orig_lat'], place['orig_long']], radius = 50000)
-    if (len(nearby['results']) > 1):
-        nearest = nearby['results'][place['index'] + 1]
-        data = {'place_id': nearest['place_id'],
-                'name': nearest['name'],
-                'latitude': nearest['geometry']['location']['lat'],
-                'longitude': nearest['geometry']['location']['lng'],
-                'rating': nearest['rating'] if 'rating' in nearest else 0,
-                'orig_lat': place['orig_lat'],
-                'orig_long': place['orig_long'],
-                'index': place['index'] + 1
-                }
+    # for i in range(0, len(nearby['results'])):
+    # if (nearby['results'][i]['place_id'] != place['place_id']):
+
+    nearest = nearby['results'][1] # [int(place['index']) + 1]
+    data = {'place_id': nearest['place_id'],
+            'name': nearest['name'],
+            'latitude': nearest['geometry']['location']['lat'],
+            'longitude': nearest['geometry']['location']['lng'],
+            'rating': nearest['rating'] if 'rating' in nearest else 0,
+            'orig_lat': place['orig_lat'],
+            'orig_long': place['orig_long'],
+            'index': place['index'] + 1
+            }
     return data
+
+
 
 
 @app.route('/describe', methods=['GET'])
@@ -175,8 +189,9 @@ def describe():
 
     request = app.current_request
     place = request.json_body
-    results = {}
+    results = {'hello': 'world'}
 
     desc = gmaps.place(place_id = place['place_id'])
+    # results.append(desc)
 
     return results
